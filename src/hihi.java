f7e7856f08d96c2d6dc997d9ebd833ea8eec9d4b
Trungdeptrai99 <ktdarkness99@gmail.com>
2020-07-04 19:29:54 +0700

    update

---
+++ src/hihi.java
@@ -0,0 +1,27 @@
+public class hihi {
+    public static void main(String[] args) {
+
+
+            begin();
+
+            System.out.println("A");
+
+            end();
+
+        }
+
+        static void begin() {
+
+            System.out.println("B");
+
+        }
+
+        static void end() {
+
+            System.out.println("C");
+
+        }
+
+}
+
+
